62ed29469158058c4f152fbba1f0092b23104fe4
Steve Allred <steve@allred.xyz>
2018-12-21 08:47:18 -0700

    Resolve arguments in SQL text

--- api/src/main/kotlin/io/xpk/apidb/ApiController.kt
+++ api/src/main/kotlin/io/xpk/apidb/ApiController.kt
@@ -1,27 +1,25 @@
 package io.xpk.apidb
 
-import org.springframework.jdbc.core.ColumnMapRowMapper
-import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.SingleColumnRowMapper
-import org.springframework.jdbc.datasource.lookup.DataSourceLookup
-import org.springframework.jdbc.datasource.lookup.DataSourceLookupFailureException
-import org.springframework.jdbc.datasource.lookup.MapDataSourceLookup
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.web.bind.annotation.*
+import java.net.URL
+import javax.servlet.http.HttpServletRequest
 
 @RestController
-class ApiController {
+class ApiController(val apiService: ApiService) {
 
-  @GetMapping
-  fun api(apiCall: ApiCall, @TenantDb tenantDb: JdbcTemplate, @ApiDb apiDb: JdbcTemplate): Any {
-    val api = apiCall.method.name + " " + apiCall.path
-    val apiText = apiDb.query("SELECT sql FROM api_sql WHERE api = ?", SingleColumnRowMapper<String>(), api)
-    if (apiText.isEmpty()) {
-      throw NotFoundException("That api, '$api', does not exist in db ${apiCall.apiDbName}.")
-    }
-    if (apiText.size > 1) {
-      throw NotFoundException("That api, '$api', has too many definitions (${apiText.size}) in db ${apiCall.apiDbName}.")
-    }
-    return tenantDb.query(apiText[0], ColumnMapRowMapper())
+  @RequestMapping
+  fun api(
+    method: HttpMethod,
+    httpEntity: HttpEntity<Map<String, Any>>,
+    webRequest: HttpServletRequest,
+    params: Map<String, String>
+  ): Any {
+    val url = URL(webRequest.requestURL.toString())
+    val args = if (method == HttpMethod.GET) params else httpEntity.body ?: hashMapOf()
+    val tenantDbName = url.host.substringBefore(".", "devdb")
+    val apiDefinitionDbName = url.host.substringBefore(".apidb", "devdb")
+    return apiService.executeApi(method, url, args, tenantDbName, apiDefinitionDbName)
   }
 }
---
+++ api/src/main/kotlin/io/xpk/apidb/ApiService.kt
@@ -0,0 +1,56 @@
+package io.xpk.apidb
+
+import org.springframework.http.HttpMethod
+import org.springframework.jdbc.core.ColumnMapRowMapper
+import org.springframework.jdbc.core.SingleColumnRowMapper
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.jdbc.datasource.lookup.DataSourceLookupFailureException
+import org.springframework.jdbc.datasource.lookup.MapDataSourceLookup
+import org.springframework.jdbc.support.GeneratedKeyHolder
+import org.springframework.stereotype.Service
+import java.net.URL
+
+@Service
+class ApiService(val dataSourceLookup: MapDataSourceLookup) {
+
+  fun executeApi(
+    method: HttpMethod,
+    url: URL,
+    args: Map<String, Any>,
+    tenantDbName: String,
+    apiDefinitionDbName: String
+  ): Any {
+    val apiText = getApiSql(apiDefinitionDbName, method, url.path)
+    return when (method) {
+      HttpMethod.GET -> jdbc(tenantDbName).query(apiText, MapSqlParameterSource(args), ColumnMapRowMapper())
+      HttpMethod.POST -> jdbc(tenantDbName).update(apiText, MapSqlParameterSource(args), GeneratedKeyHolder())
+      HttpMethod.PUT -> jdbc(tenantDbName).update(apiText, MapSqlParameterSource(args))
+      else -> throw UserErrorException("I don't know how to handle a $method")
+    }
+  }
+
+  private fun getApiSql(apiDefinitionDbName: String, method: HttpMethod, api: String): String {
+    val apiText =
+      jdbc(apiDefinitionDbName).jdbcTemplate.query(
+        "SELECT sql FROM api_sql WHERE api = ?",
+        SingleColumnRowMapper<String>(),
+        method.name + " " + api
+      )
+    if (apiText.isEmpty()) {
+      throw NotFoundException("That api, '$api', does not exist in db '$apiDefinitionDbName'.")
+    }
+    if (apiText.size > 1) {
+      throw NotFoundException("That api, '$api', has too many definitions (${apiText.size}) in db '$apiDefinitionDbName'.")
+    }
+    return apiText[0]
+  }
+
+  fun jdbc(dbName: String): NamedParameterJdbcTemplate {
+    return try {
+      NamedParameterJdbcTemplate(dataSourceLookup.getDataSource(dbName))
+    } catch (ex: DataSourceLookupFailureException) {
+      throw UserErrorException("That db, '$dbName', does not exist.")
+    }
+  }
+}
